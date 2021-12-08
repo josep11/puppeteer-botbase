c4e5c8bbbd1f15d3997bd324262433f09da47b77
Josep Alsina <josepalsinasio@gmail.com>
2021-12-08 12:47:00 +0100

    HTMLMarkupChangedError

--- custom_errors.js
+++ custom_errors.js
@@ -26,7 +26,16 @@ class NotImplementedError extends Error {
 }
 
 class NoInternetError extends Error {
-    constructor(message='No Tens Internet ❌') {
+    constructor(message = 'No Tens Internet ❌') {
+        super(message)
+        Error.captureStackTrace(this, this.constructor);
+
+        this.name = this.constructor.name
+    }
+}
+
+class HTMLMarkupChangedError extends Error {
+    constructor(message = 'Markup: The page HTML changed. Need to fix it') {
         super(message)
         Error.captureStackTrace(this, this.constructor);
 
@@ -38,5 +47,6 @@ module.exports = {
     LoginError,
     MyTimeoutError,
     NoInternetError,
-    NotImplementedError
+    NotImplementedError,
+    HTMLMarkupChangedError
 }
