d1aa3706d522f9200bac2139c1121716167442f3
Josep Alsina <josepalsinasio@gmail.com>
2022-03-18 16:38:31 +0100

    .

--- hello.js
+++ hello.js
@@ -3,19 +3,22 @@ const glob = require("glob");
 console.log(getLocalPuppeteerInstallation());
 
 function getLocalPuppeteerInstallation() {
-    // TODO: port it to other OS's
-    // fails on circleci
+
     // const puppeteerDirname = './node_modules/puppeteer/.local-chromium/mac-*/chrome-mac/Chromium.app/Contents/MacOS/Chromium'
     // example path for linux:
     // './node_modules/puppeteer/.local-chromium/linux-970485/chrome-linux/chrome'
+    // './node_modules/puppeteer/.local-chromium/linux-624492/chrome-linux'
     // example path for mac (tested and working):
-    // './node_modules/puppeteer/.local-chromium/mac-*/chrome-mac/Chromium.app/Contents/MacOS/Chromium'
-    const puppeteerDirname = './node_modules/puppeteer/.local-chromium/linux-*/chrome-linux/chrome'
-    '/web/node_modules/prerender-spa-plugin/node_modules/puppeteer/.local-chromium/linux-624492/chrome-linux'
-    const results = glob.sync(puppeteerDirname);
-    console.log(results);
-    if (!results || results.length < 1) {
-        throw 'Puppeteer not installed'
+    const puppeteerDirnameMac = './node_modules/puppeteer/.local-chromium/mac-*/chrome-mac/Chromium.app/Contents/MacOS/Chromium'
+    const puppeteerDirnameLinux = './node_modules/puppeteer/.local-chromium/linux-*/chrome-linux/chrome'
+    const puppeteerDirnames = [puppeteerDirnameLinux, puppeteerDirnameMac];
+    for (const puppeteerDirname of puppeteerDirnames) {
+        const results = glob.sync(puppeteerDirname);
+        console.log(results);
+        if (!results || results.length < 1) {
+            return results[0];
+        }
     }
-    return results[0];
+
+    throw 'Puppeteer not installed';
 }
