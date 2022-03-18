dd8efb46a4435a1e642ea0f6f888df02a560bf53
Josep Alsina <josepalsinasio@gmail.com>
2022-03-18 13:35:40 +0100

    .

--- hello.js
+++ hello.js
@@ -1,1 +1,16 @@
-console.log('hi world');
+const glob = require("glob");
+
+console.log(getLocalPuppeteerInstallation());
+
+function getLocalPuppeteerInstallation() {
+    // TODO: port it to other OS's
+    // fails on circleci
+    const puppeteerDirname = './node_modules/puppeteer/.local-chromium/*'
+    // '/web/node_modules/prerender-spa-plugin/node_modules/puppeteer/.local-chromium/linux-624492/chrome-linux'
+    const results = glob.sync(puppeteerDirname);
+    console.log(results);
+    if (!results || results.length < 1) {
+        throw 'Puppeteer not installed'
+    }
+    return results[0];
+}
