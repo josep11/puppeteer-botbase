800b59258af32029a0ab32f2b5bcf8e246ea0c62
Josep Alsina <josepalsinasio@gmail.com>
2022-03-18 16:34:05 +0100

    .

--- hello.js
+++ hello.js
@@ -6,7 +6,11 @@ function getLocalPuppeteerInstallation() {
     // TODO: port it to other OS's
     // fails on circleci
     // const puppeteerDirname = './node_modules/puppeteer/.local-chromium/mac-*/chrome-mac/Chromium.app/Contents/MacOS/Chromium'
-    const puppeteerDirname = './node_modules/puppeteer/.local-chromium/*/*/*'
+    // example path for linux:
+    // './node_modules/puppeteer/.local-chromium/linux-970485/chrome-linux/chrome'
+    // example path for mac (tested and working):
+    // './node_modules/puppeteer/.local-chromium/mac-*/chrome-mac/Chromium.app/Contents/MacOS/Chromium'
+    const puppeteerDirname = './node_modules/puppeteer/.local-chromium/linux-*/chrome-linux/chrome/*'
     '/web/node_modules/prerender-spa-plugin/node_modules/puppeteer/.local-chromium/linux-624492/chrome-linux'
     const results = glob.sync(puppeteerDirname);
     console.log(results);
