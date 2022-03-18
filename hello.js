a22b93ff5daa40c76d67e9c297fd9665f22db1a6
Josep Alsina <josepalsinasio@gmail.com>
2022-03-18 16:35:25 +0100

    e

--- hello.js
+++ hello.js
@@ -10,7 +10,7 @@ function getLocalPuppeteerInstallation() {
     // './node_modules/puppeteer/.local-chromium/linux-970485/chrome-linux/chrome'
     // example path for mac (tested and working):
     // './node_modules/puppeteer/.local-chromium/mac-*/chrome-mac/Chromium.app/Contents/MacOS/Chromium'
-    const puppeteerDirname = './node_modules/puppeteer/.local-chromium/linux-*/chrome-linux/chrome/*'
+    const puppeteerDirname = './node_modules/puppeteer/.local-chromium/linux-*/chrome-linux/chrome'
     '/web/node_modules/prerender-spa-plugin/node_modules/puppeteer/.local-chromium/linux-624492/chrome-linux'
     const results = glob.sync(puppeteerDirname);
     console.log(results);
