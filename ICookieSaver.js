643da9af69c4f9d39054b3b3ffdd8eb3d846d9ef
Josep Alsina <josep.alsina@addevent.com>
2022-10-18 20:46:13 +0200

    added type hints for cookie saver

--- ICookieSaver.js
+++ ICookieSaver.js
@@ -1,29 +1,31 @@
-const { NotImplementedError } = require('./custom_errors');
+const { NotImplementedError } = require("./custom_errors");
 
 class ICookieSaver {
+	constructor() {}
 
-    constructor() {
+	/**
+	 * @param {*} cookies
+	 */
+	async writeCookies(cookies) {
+		if (!cookiesJson) {
+			throw new Error("Developer fix this: unexistent param");
+		}
+		throw new NotImplementedError("not implemented");
+	}
 
-    }
+	/**
+	 * @returns {Promise<*>}
+	 */
+	async readCookies() {
+		throw new NotImplementedError("not implemented");
+	}
 
-    async readCookies() {
-        throw new NotImplementedError('not implemented');
-    }
-
-    async writeCookies(cookiesJson) {
-        if (!cookiesJson) {
-            throw new Error('Developer fix this: unexistent param');
-        }
-        throw new NotImplementedError('not implemented');
-    }
-
-    /**
-     * When extending it, should save "[]" as empty cookie
-     */
-    async removeCookies() {
-        throw new NotImplementedError('not implemented');
-    }
-
+	/**
+	 * When extending it, should save "[]" as empty cookie
+	 */
+	async removeCookies() {
+		throw new NotImplementedError("not implemented");
+	}
 }
 
 module.exports = ICookieSaver;
--- LocalFsCookieSaver.js
+++ LocalFsCookieSaver.js
@@ -1,56 +1,57 @@
-const fs = require('fs');
-const { dirname } = require('path');
-const helper = require('./helper');
-const ICookieSaver = require('./ICookieSaver');
+const fs = require("fs");
+const { dirname } = require("path");
+const helper = require("./helper");
+const ICookieSaver = require("./ICookieSaver");
 
 class LocalFsCookieSaver extends ICookieSaver {
+	/**
+	 *
+	 * @param {Object} param should have the following keys { cookiesFilePath }
+	 */
+	constructor({ cookiesFilePath }) {
+		super();
+		if (!cookiesFilePath || typeof cookiesFilePath != "string") {
+			throw new Error(
+				"Developer fix this: cookiesFilePath is undefined or not string"
+			);
+		}
 
-    /**
-     * 
-     * @param {Object} param should have the following keys { cookiesFilePath }
-     */
-    constructor({ cookiesFilePath }) {
-        super();
-        if (!cookiesFilePath || typeof cookiesFilePath != "string") {
-            throw new Error('Developer fix this: cookiesFilePath is undefined or not string');
-        }
+		this.cookiesFilePath = cookiesFilePath;
 
-        this.cookiesFilePath = cookiesFilePath;
+		helper.createDirIfNotExists(dirname(this.cookiesFilePath));
+	}
 
-        helper.createDirIfNotExists(dirname(this.cookiesFilePath))
-    }
+	/**
+	 *
+	 * @returns {Promise<*>}
+	 */
+	async readCookies() {
+		try {
+			const jsonString = fs.readFileSync(this.cookiesFilePath);
+			return JSON.parse(jsonString);
+		} catch (err) {
+			console.error("Reading cookie error. Defaulting to [] \n\n" + err);
+			return [];
+		}
+	}
 
-    async readCookies() {
-        try {
-            const jsonString = fs.readFileSync(this.cookiesFilePath);
-            const cookies = JSON.parse(jsonString);
-            return cookies;
-        } catch (err) {
-            console.error("Reading cookie error. Defaulting to [] \n\n" + err)
-            return [];
-        }
-    }
+	/**
+	 * @param {*} cookies
+	 */
+	async writeCookies(cookies) {
+		let cookiesText = cookies;
+		if (typeof cookies == "object") {
+			cookiesText = JSON.stringify(cookies, null, 2);
+		}
+		return fs.writeFileSync(this.cookiesFilePath, cookiesText);
+	}
 
-    /**
-     * 
-     * @param {*} cookies
-     * @returns 
-     */
-    async writeCookies(cookies) {
-        let cookiesText = cookies;
-        if (typeof cookies == "object") {
-            cookiesText = JSON.stringify(cookies, null, 2);
-        }
-        return fs.writeFileSync(this.cookiesFilePath, cookiesText);
-    }
-
-    /**
-     * When extending it, should save "[]" as empty cookie
-     */
-    async removeCookies() {
-        await this.writeCookies('[]');
-    }
-
+	/**
+	 * When extending it, should save "[]" as empty cookie
+	 */
+	async removeCookies() {
+		await this.writeCookies("[]");
+	}
 }
 
 module.exports = LocalFsCookieSaver;
