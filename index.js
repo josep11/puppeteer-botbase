4aea4c59d97c8c93b966f779c46c2effb50c8d05
Josep Alsina <josep.alsina@addevent.com>
2024-02-25 23:07:13 +0100

    added imports in index.js

--- index.js
+++ index.js
@@ -1,3 +1,38 @@
+const {
+    LoginError,
+    MyTimeoutError,
+    NoInternetError,
+    NotImplementedError,
+    HTMLMarkupChangedError
+} = require('./src/custom_errors');
+
+const HelperPuppeteer = require('./src/helper_puppeteer');
+const helper = require('./src/helper');
+const ICookieSaver = require('./src/ICookieSaver');
+const IScreenshotSaver = require('./src/IScreenshotSaver');
+const LocalFsCookieSaver = require('./src/LocalFsCookieSaver');
+const LocalScreenshotSaver = require('./src/LocalScreenshotSaver');
+
 module.exports = {
+    // BotBaseFactory
+    BotBaseFactory: require('./src/botbase-factory'),
+
+    // Errors
+    LoginError,
+    MyTimeoutError,
+    NoInternetError,
+    NotImplementedError,
+    HTMLMarkupChangedError,
+
+    // Helper Puppeteer
+    HelperPuppeteer,
+
+    // Helper
+    helper,
     
+    // Savers
+    ICookieSaver,
+    IScreenshotSaver,
+    LocalFsCookieSaver,
+    LocalScreenshotSaver,
 };
